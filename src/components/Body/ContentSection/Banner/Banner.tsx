849f8c4603506b37efdb86a2b48fccbfa07d1ccf
bhawna kumari <singhbhawna2308@gmail.com>
2025-09-19 21:20:20 +0530

    code changes

--- src/components/Body/ContentSection/Banner/Banner.tsx
+++ src/components/Body/ContentSection/Banner/Banner.tsx
@@ -1,28 +1,3 @@
-// import bannerImg from "../../../../assets/img/bannerShoes.svg"
-// import { bannerList } from "./BannerList"
-// export default function Banner() {
-//     return(
-//         <>
-//         <div className="bannerWrapper">
-//             <div className="BannerLeft">
-//                 <div className="BannerHeadline">
-//                     <h1>Adidas Men Running Sneakers</h1>
-//                 </div>
-//                 <div className="BannerDetails">
-//                     <p>Performance and design. Taken right to the edge.</p>
-//                 </div>
-//                 <div className="BannerButton">
-//                     <span>Shop Now</span>
-//                 </div>
-//             </div>
-//             <div className="BannerRight">
-//                 <img src={bannerImg} alt="Banner Image" />
-//             </div>
-//         </div>
-//         </>
-//     )
-// }
-
 import { useState, useEffect } from "react";
 import { bannerList } from "./BannerList";
 
