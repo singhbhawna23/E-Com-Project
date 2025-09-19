0a707d7216d2332886144329c7d7c6bc8d57a284
bhawna kumari <singhbhawna2308@gmail.com>
2025-09-19 19:58:40 +0530

    pagination added

---
+++ src/components/Body/ContentSection/Banner/BannerList.tsx
@@ -0,0 +1,7 @@
+export const bannerList = [
+    {id:1, headline:"Adidas Men Running Sneakers", detail:"Performance and design. Taken right to the edge.", btn:"Shop Now", img:"src/assets/img/bannerShoes.svg"},
+    {id:2, headline:"Nike Men Running Sneakers", detail:"Performance and design. Taken right to the edge.", btn:"Shop Now", img:"src/assets/img/bannerShoes.svg"},
+    {id:3, headline:"Puma Men Running Sneakers", detail:"Performance and design. Taken right to the edge.", btn:"Shop Now", img:"src/assets/img/bannerShoes.svg"},
+    {id:4, headline:"Reebok Men Running Sneakers", detail:"Performance and design. Taken right to the edge.", btn:"Shop Now", img:"src/assets/img/bannerShoes.svg"},
+    {id:5, headline:"Skechers Men Running Sneakers", detail:"Performance and design. Taken right to the edge.", btn:"Shop Now", img:"src/assets/img/bannerShoes.svg"},
+]
