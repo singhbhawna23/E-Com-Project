05acc947a15f28948bd44650958d3dcf6f87c9a0
bhawna kumari <singhbhawna2308@gmail.com>
2025-09-19 20:05:35 +0530

    pagination added

--- src/components/Body/SidePanel/SidePanel.tsx
+++ src/components/Body/SidePanel/SidePanel.tsx
@@ -2,12 +2,14 @@ import Brand from "./Brand/Brand"
 import HotDeals from "./HotDeals/HotDeals"
 import More from "./More/More"
 import Prices from "./Prices/Prices"
+import Color from "./Color/Color"
 export default function SidePanel() {
     return(
         <>
         <div className="sidePanelWrapper">
             <HotDeals/>
             <Prices/>
+            <Color/>
             <Brand/>
             <More/>
         </div>
