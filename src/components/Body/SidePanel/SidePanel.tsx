0286d3a946a89d45d95e76012ee324d51688f510
bhawna kumari <singhbhawna2308@gmail.com>
2025-09-19 20:14:43 +0530

    code changes

--- src/components/Body/SidePanel/SidePanel.tsx
+++ src/components/Body/SidePanel/SidePanel.tsx
@@ -1,18 +1,46 @@
-import Brand from "./Brand/Brand"
-import HotDeals from "./HotDeals/HotDeals"
-import More from "./More/More"
-import Prices from "./Prices/Prices"
-import Color from "./Color/Color"
-export default function SidePanel() {
-    return(
-        <>
-        <div className="sidePanelWrapper">
-            <HotDeals/>
-            <Prices/>
-            <Color/>
-            <Brand/>
-            <More/>
-        </div>
-        </>
-    )
+// import Brand from "./Brand/Brand"
+// import HotDeals from "./HotDeals/HotDeals"
+// import More from "./More/More"
+// import Prices from "./Prices/Prices"
+// import Color from "./Color/Color"
+// export default function SidePanel() {
+//     return(
+//         <>
+//         <div className="sidePanelWrapper">
+//             <HotDeals/>
+//             <Prices/>
+//             <Color/>
+//             <Brand/>
+//             <More/>
+//         </div>
+//         </>
+//     )
+// }
+
+import Brand from "./Brand/Brand";
+import HotDeals from "./HotDeals/HotDeals";
+import More from "./More/More";
+import Prices from "./Prices/Prices";
+import Color from "./Color/Color";
+
+export default function SidePanel({
+  selectedBrands,
+  onBrandChange,
+  selectedHotDeals,
+  onHotDealsChange,
+  selectedColors,
+  onColorChange,
+}) {
+  return (
+    <div className="sidePanelWrapper">
+      <HotDeals
+        selectedHotDeals={selectedHotDeals}
+        onHotDealsChange={onHotDealsChange}
+      />
+      <Prices />
+      <Color selectedColors={selectedColors} onColorChange={onColorChange} />
+      <Brand selectedBrands={selectedBrands} onBrandChange={onBrandChange} />
+      <More />
+    </div>
+  );
 }
